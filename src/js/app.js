11b990757f66c76d3a65619240d9c1c6ef345d3e
Eineleine <dsa210487@mail.ru>
2024-11-10 04:32:58 +0300

    appveyor.yml is change

--- src/js/app.js
+++ src/js/app.js
@@ -1,23 +1,24 @@
 import image from '../js/goblin.png';
 
-document.addEventListener('DOMContentLoaded', () => {
-    const field = document.querySelector('.field');
-    const fieldBoard = [];
-    const imgGoblin = document.createElement('img');
-    imgGoblin.src = image;
 
-    for (let i = 0; i < 16; i++) {
-        let newDiv = document.createElement('div');
-        field.append(newDiv);
-        fieldBoard.push(newDiv);
-    }
-    
-    let currentField = 0;
+    document.addEventListener('DOMContentLoaded', () => {
+        const field = document.querySelector('.field');
+        const fieldBoard = [];
+        const imgGoblin = document.createElement('img');
+        imgGoblin.src = image;
 
-    function goblin() {
-        fieldBoard[currentField].innerHTML = '';
-        currentField = Math.floor(Math.random() * fieldBoard.length);
-        fieldBoard[currentField].append(imgGoblin);
-    }
-    setInterval(goblin, 1000);
-});
+        for (let i = 0; i < 16; i++) {
+            let newDiv = document.createElement('div');
+            field.append(newDiv);
+            fieldBoard.push(newDiv);
+        }
+        
+        let currentField = 0;
+
+        function goblin() {
+            fieldBoard[currentField].innerHTML = '';
+            currentField = Math.floor(Math.random() * fieldBoard.length);
+            fieldBoard[currentField].append(imgGoblin);
+        }
+        setInterval(goblin, 1000);
+    });
